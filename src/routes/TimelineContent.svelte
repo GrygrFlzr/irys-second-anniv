81cce468e2f8e751e8d77a92cee465f227348d1b
Walt280 <32001362+Walt280@users.noreply.github.com>
2023-06-14 00:46:06 -0400

    Fix date in timeline stuff whoops.

--- src/routes/TimelineContent.svelte
+++ src/routes/TimelineContent.svelte
@@ -62,7 +62,7 @@
 		let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
 		let mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(date);
 		let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
-		return `${da} ${mo}, ${ye}`;
+		return `${mo} ${da}, ${ye}`;
 	}
 </script>
 
