da12eb55ab7c189f14d06944c72657563eb0e3ff
ccoddes <ccoddes@gmail.com>
2023-06-17 15:46:43 +0800

    Initial framework for Art page
    - Setup art page structure and placeholder items
    - Format app.html with formatter

---
+++ src/routes/art/ArtGalleryItem.svelte
@@ -0,0 +1,10 @@
+<div class="content">
+	<div class="item-img">My Art Piece Here</div>
+	<div class="item-contributor">Contributor's Name</div>
+</div>
+
+<style>
+	.content {
+		padding: 24px 0px;
+	}
+</style>
