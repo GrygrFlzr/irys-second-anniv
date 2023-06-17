ba03a2dfb08981b5a5e4ee798e98f30a3f30b3ed
Walt280 <32001362+Walt280@users.noreply.github.com>
2023-06-17 15:10:06 -0400

    Update event schema with restricted subset of richtext elements.

--- src/schema/Events.ts
+++ src/schema/Events.ts
@@ -1,67 +1,63 @@
-import { CollectionConfig } from "payload/types";
+import { CollectionConfig } from 'payload/types';
 
 const Events: CollectionConfig = {
-  slug: "events",
-  labels: {
-    singular: "Event",
-    plural: "Events",
-  },
-  access: {
-    read: () => true,
-  },
-  fields: [
-    {
-      name: "date",
-      type: "date",
-      required: true,
-    },
-    {
-      name: "title",
-      type: "text",
-      required: true,
-    },
-    {
-      name: "images",
-      type: "array",
-      required: true,
-      fields: [
-        {
-          name: "image",
-          type: "upload",
-          relationTo: "events-media",
-          filterOptions: {
-            mimeType: {
-              or: [
-                { equals: "image/jpeg" },
-                { equals: "image/png" },
-                { equals: "image/bmp" },
-              ],
-            },
-          },
-        }
-      ],
-    },
-    {
-      name: "background_image",
-      type: "upload",
-      required: false,
-      relationTo: "events-media",
-      filterOptions: {
-        mimeType: {
-          or: [
-            { equals: "image/jpeg" },
-            { equals: "image/png" },
-            { equals: "image/bmp" },
-          ],
-        },
-      },
-    },
-    {
-      name: "content",
-      type: "richText",
-      required: true,
-    },
-  ],
+	slug: 'events',
+	labels: {
+		singular: 'Event',
+		plural: 'Events'
+	},
+	access: {
+		read: () => true
+	},
+	fields: [
+		{
+			name: 'date',
+			type: 'date',
+			required: true
+		},
+		{
+			name: 'title',
+			type: 'text',
+			required: true
+		},
+		{
+			name: 'images',
+			type: 'array',
+			required: true,
+			fields: [
+				{
+					name: 'image',
+					type: 'upload',
+					relationTo: 'events-media',
+					filterOptions: {
+						mimeType: {
+							or: [{ equals: 'image/jpeg' }, { equals: 'image/png' }, { equals: 'image/bmp' }]
+						}
+					}
+				}
+			]
+		},
+		{
+			name: 'background_image',
+			type: 'upload',
+			required: false,
+			relationTo: 'events-media',
+			filterOptions: {
+				mimeType: {
+					or: [{ equals: 'image/jpeg' }, { equals: 'image/png' }, { equals: 'image/bmp' }]
+				}
+			}
+		},
+		{
+			name: 'content',
+			type: 'richText',
+			required: true,
+			admin: {
+				elements: ['link', 'blockquote', 'ul', 'ol', 'indent'],
+				leaves: ['bold', 'italic', 'underline', 'strikethrough']
+			}
+		}
+	]
 };
 
 export default Events;
