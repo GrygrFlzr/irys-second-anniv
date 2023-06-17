4bd882b77024f5cdde2efac6f4344cfba34bb1bc
ccoddes <ccoddes@gmail.com>
2023-06-17 15:46:43 +0800

    Initial framework for Art page
    - Setup art page structure and placeholder items
    - Format app.html with formatter

---
+++ src/routes/art/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	import ArtGallery from './ArtGallery.svelte';
+</script>
+
+<div class="background">
+	<div class="content">
+		<div class="header-group">
+			<div class="header-title">Anniversary Messages and Fanart</div>
+			<div class="header-desc">
+				A collection of messages and art from IRyStocrats around the world, for IRyS' 2nd
+				Anniversary.
+			</div>
+		</div>
+
+		<div class="gallery-container">
+			<ArtGallery />
+		</div>
+	</div>
+</div>
+
+<style>
+	.background {
+		color: black;
+	}
+
+	.content {
+		padding-top: 48px;
+	}
+
+	.header-group {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		margin: auto;
+		width: 50em;
+	}
+
+	.header-title {
+		color: #ffffff;
+		font-size: 40px;
+	}
+
+	.header-desc {
+		color: #ddd;
+		font-size: 24px;
+		margin-top: 24px;
+		text-align: center;
+	}
+
+	.gallery-container {
+		margin-top: 48px;
+	}
+</style>
