30853d042e33dbc4aaba751378a7da83bb2c2602
ccoddes <ccoddes@gmail.com>
2023-06-17 17:30:29 +0800

    Add placeholder art and display data in gallery

--- src/routes/art/ArtGalleryItem.svelte
+++ src/routes/art/ArtGalleryItem.svelte
@@ -1,10 +1,26 @@
+<script>
+	export let contributor = 'contributor';
+	export let images = ['/img/art/art-1.png'];
+</script>
+
 <div class="content">
-	<div class="item-img">My Art Piece Here</div>
-	<div class="item-contributor">Contributor's Name</div>
+	<div>
+		{#each images as image}
+			<img class="item-img" src={image} alt="Art by {contributor}" />
+		{/each}
+	</div>
+	<div class="item-contributor">
+		{contributor}
+	</div>
 </div>
 
 <style>
 	.content {
 		padding: 24px 0px;
+	}
+
+	.item-contributor {
+		font-size: 20px;
+		margin-top: 12px;
 	}
 </style>
