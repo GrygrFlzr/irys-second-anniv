2f04536388d1c1e5c3a18cb2247900ab711e32bf
Lyu, Wei Da <b00605009@ntu.edu.tw>
2023-06-23 08:59:13 +0800

    use moduleResolution bundler for photoswipe/lightbox

--- src/ambient.d.ts
+++
@@ -1,6 +0,0 @@
-// type definitions seems to be wrong with "moduleResolution": "node"
-// https://arethetypeswrong.github.io/?p=photoswipe%405.3.7
-declare module 'photoswipe/lightbox' {
-	import PhotoSwipeLightBox from 'photoswipe/dist/types/lightbox/lightbox';
-	export default PhotoSwipeLightBox;
-}
