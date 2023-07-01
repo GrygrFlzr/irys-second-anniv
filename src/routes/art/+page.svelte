026228c63cd9f6b020aa5db1c3e3047b62360c9d
Walt280 <32001362+Walt280@users.noreply.github.com>
2023-07-01 18:44:48 -0400

    Add the data variable for art page.

---
+++ src/routes/art/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	// Prefilled by server data; assuming the data is sorted by server side code.
+	// Data from the server is autofilled into this variable.
+	// Access the actual data with the "data" member.
+	export let data: PageData;
+</script>
