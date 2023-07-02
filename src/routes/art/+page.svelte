87292b3eeac1140f60b98681c795a1acdc2135fb
Walt <32001362+Walt280@users.noreply.github.com>
2023-07-01 23:45:41 -0400

    Art page cms integration (#7)

--- src/routes/art/+page.svelte
+++ src/routes/art/+page.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
 	import ArtGallery from './ArtGallery.svelte';
+	import type { PageData } from './$types';
+
+	// Prefilled by server data; assuming the data is sorted by server side code.
+	// Data from the server is autofilled into this variable.
+	// Access the actual data with the "data" member.
+	export let data: PageData;
 
 	import { fade, fly } from 'svelte/transition';
 
